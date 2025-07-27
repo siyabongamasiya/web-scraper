93afbad9b7ce1babe2845e5ceec6f84a08893937
Siyabonga <siyabongakhanyile76@gmail.com>
2025-07-27 18:54:39 +0200

    project complete

---
+++ scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+def putValuesInExcel(name,description,habitat,lifespan,diet) :
+    activeWorkSheet.append([name,fact,habitat,lifespan,diet])
+
+    
+wb = Workbook()
+
+# Send a GET request
+url = "https://test-scrape-site.onrender.com/animals.html"
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Failed to fetch the webpage.")
+    exit()
+
+# Parse the HTML
+soup = BeautifulSoup(response.text, "html.parser")
+
+#find the list 
+animals = soup.find_all(class_="animal-card")
+
+activeWorkSheet = wb.active
+for animal in animals :
+    name = animal.find('h2').text
+    fact = animal.find(class_="fact").text
+    habitat = animal.find(class_="habitat").text
+    lifespan = animal.find(class_="lifespan").text
+    diet = animal.find(class_="diet").text
+    putValuesInExcel(name,fact,habitat,lifespan,diet)
+
+wb.save("animals.xlsx")
+
+
